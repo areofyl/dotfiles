5b3f0b4a6b623a8f6515d5054a01c919200486d1
areofyl <areofyl@users.noreply.github.com>
2026-04-05 10:11:40 -0700

    update nvim colorscheme

--- gentoo/.config/nvim/lua/plugins/colorscheme.lua
+++ gentoo/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
-    lazy = false,
     priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "medium",
-      })
+    opts = {},
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
       vim.cmd.colorscheme("gruvbox")
     end,
   },
