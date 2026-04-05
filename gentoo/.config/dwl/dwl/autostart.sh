57404f8aec872760dfb59b042888576f204b0a9c
areofyl <areofyl@users.noreply.github.com>
2026-04-05 10:13:18 -0700

    sync .config/dwl/dwl/autostart.sh

---
+++ gentoo/.config/dwl/dwl/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# clean up stale state
+pkill -f 'tail.*dwl-status' 2>/dev/null
+pkill -f 'cat /tmp/dwl-status' 2>/dev/null
+rm -f /tmp/dwl-status
+
+# start dwl (status is piped to startup.sh's stdin, which writes it to /tmp/dwl-status)
+dwl -s ~/.config/dwl/startup.sh 2>/tmp/dwl-error.log
