b150166a481b8009e40c98b92f1de5893efd5fd8
areofyl <areofyl@users.noreply.github.com>
2026-04-05 10:11:40 -0700

    update nvim lsp config

--- gentoo/.config/nvim/lua/plugins/lsp.lua
+++ gentoo/.config/nvim/lua/plugins/lsp.lua
@@ -1,142 +1,67 @@
 return {
-  -- Completion
+  -- Mason: portable package manager for LSP servers, formatters, etc.
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-
-      -- Configure LSP servers using native vim.lsp.config (nvim 0.11+)
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      vim.lsp.config("clangd", {
-        cmd = { "clangd" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_markers = { "compile_commands.json", ".clangd", ".git" },
-        capabilities = capabilities,
-      })
-
-      vim.lsp.config("pyright", {
-        cmd = { "pyright-langserver", "--stdio" },
-        filetypes = { "python" },
-        root_markers = { "pyproject.toml", "setup.py", "pyrightconfig.json", ".git" },
-        capabilities = capabilities,
-      })
-
-      vim.lsp.config("gopls", {
-        cmd = { "gopls" },
-        filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        root_markers = { "go.mod", ".git" },
-        capabilities = capabilities,
-      })
-
-      vim.lsp.enable({ "clangd", "pyright", "gopls" })
-    end,
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    opts = {},
   },
 
-  -- LSP keymaps
+  -- Auto-configure LSP servers installed by Mason
   {
-    "hrsh7th/cmp-nvim-lsp",
-    lazy = true,
-    init = function()
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      automatic_enable = true,
+    },
+  },
+
+  -- Declaratively install tools via Mason
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "pylsp",
+        "clang-format",
+        "ruff",
+        "codelldb",
+        "debugpy",
+      },
+    },
+  },
+
+  -- LSP config
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      -- Diagnostics
+      vim.diagnostic.config({
+        virtual_text = { spacing = 4 },
+        severity_sort = true,
+        signs = true,
+        underline = true,
+      })
+
+      -- Keymaps on LspAttach
       vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(ev)
+        callback = function(args)
+          local buf = args.buf
           local map = function(mode, lhs, rhs, desc)
-            vim.keymap.set(mode, lhs, rhs, { buffer = ev.buf, desc = desc })
+            vim.keymap.set(mode, lhs, rhs, { buffer = buf, desc = desc })
           end
+
           map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
           map("n", "gr", vim.lsp.buf.references, "References")
-          map("n", "K", vim.lsp.buf.hover, "Hover docs")
+          map("n", "K", vim.lsp.buf.hover, "Hover")
           map("n", "<leader>cr", vim.lsp.buf.rename, "Rename")
           map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+          map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
         end,
-      })
-    end,
-  },
-
-  -- Diagnostics
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
-      { "<leader>e", vim.diagnostic.open_float, desc = "Show diagnostic" },
-      { "[d", vim.diagnostic.goto_prev, desc = "Prev diagnostic" },
-      { "]d", vim.diagnostic.goto_next, desc = "Next diagnostic" },
-    },
-    config = function()
-      require("trouble").setup()
-    end,
-  },
-
-  -- Formatting
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          c = { "clang-format" },
-          cpp = { "clang-format" },
-          python = { "black" },
-          go = { "gofmt" },
-          lua = { "stylua" },
-        },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
       })
     end,
   },
