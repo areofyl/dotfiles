aa3978357c6f4971969caefc4525b7952cc5b7ac
areofyl <areofyl@users.noreply.github.com>
2026-04-08 19:02:05 -0700

    sync .config/dwl/waybar-tags.sh

--- gentoo/.config/dwl/waybar-tags.sh
+++ gentoo/.config/dwl/waybar-tags.sh
@@ -16,20 +16,20 @@ while read -r line; do
             for i in 1 2 3 4 5 6; do
                 bit=$((1 << (i - 1)))
                 if [ -n "$out" ]; then
-                    out="$out <span color='#4a3f34'>|</span> "
+                    out="$out <span color='#2e3642'>|</span> "
                 fi
                 if [ $((sel & bit)) -ne 0 ]; then
-                    out="$out<span color='#c08060'>$i</span>"
+                    out="$out<span color='#8a95b0'>$i</span>"
                 elif [ $((urg & bit)) -ne 0 ]; then
                     out="$out<span color='#ff0000'>$i</span>"
                 else
-                    out="$out<span color='#7a6e62'>$i</span>"
+                    out="$out<span color='#5a6470'>$i</span>"
                 fi
             done
             for i in 7 8 9; do
                 bit=$((1 << (i - 1)))
                 if [ $((sel & bit)) -ne 0 ]; then
-                    out="$out <span color='#4a3f34'>|</span> <span color='#c08060'>$i</span>"
+                    out="$out <span color='#2e3642'>|</span> <span color='#8a95b0'>$i</span>"
                 fi
             done
             printf '{"text": "%s"}\n' "$out"
