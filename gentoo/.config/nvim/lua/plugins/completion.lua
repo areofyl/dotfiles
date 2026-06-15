829aad76132f2acfa46d17f70463432abc6e445a
areofyl <areofyl@users.noreply.github.com>
2026-06-15 12:24:39 -0700

    sync .config/nvim/lua/plugins/completion.lua

--- gentoo/.config/nvim/lua/plugins/completion.lua
+++ gentoo/.config/nvim/lua/plugins/completion.lua
@@ -1,20 +1,52 @@
 return {
   {
-    "saghen/blink.cmp",
-    version = "1.*",
+    "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-    opts = {
-      keymap = { preset = "default" },
-      completion = {
-        keyword = { range = "full" },
-        list = {
-          selection = { preselect = true, auto_insert = false },
-        },
-        menu = { auto_show = true },
-      },
-      sources = {
-        default = { "lsp", "snippets", "path" },
-      },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
     },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-e>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        }),
+      })
+    end,
   },
 }
