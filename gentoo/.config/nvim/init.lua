ff178dc41a5bb9ce6cfdd348ea483cde7b7821b6
areofyl <areofyl@users.noreply.github.com>
2026-06-15 12:24:39 -0700

    sync .config/nvim/init.lua

--- gentoo/.config/nvim/init.lua
+++ gentoo/.config/nvim/init.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.cmd.colorscheme("vim")
+-- colorscheme set by plugins/colorscheme.lua
+vim.opt.number = true
+vim.opt.relativenumber = true
 require("config.lazy")
