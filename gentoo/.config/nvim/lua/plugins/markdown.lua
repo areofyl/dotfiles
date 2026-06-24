e1c49f97b54b89b5f56776b941609e8e16bb3c60
areofyl <areofyl@users.noreply.github.com>
2026-06-23 20:00:31 -0700

    sync .config/nvim/lua/plugins/markdown.lua

--- gentoo/.config/nvim/lua/plugins/markdown.lua
+++ gentoo/.config/nvim/lua/plugins/markdown.lua
@@ -1,1 +1,8 @@
-return {}
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = "markdown",
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+}
