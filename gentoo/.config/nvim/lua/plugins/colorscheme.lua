e5305a084a70a3095ae5e3e1d8713255e00b7883
areofyl <areofyl@users.noreply.github.com>
2026-07-13 21:00:14 -0700

    sync .config/nvim/lua/plugins/colorscheme.lua

--- gentoo/.config/nvim/lua/plugins/colorscheme.lua
+++ gentoo/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,12 @@
 return {
-  "vague2k/vague.nvim",
-  priority = 1000,
-  opts = {},
+  {
+    "vague2k/vague.nvim",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {},
+  },
 }
