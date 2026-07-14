e7269956ee274a5b08794bf386dbd8a82cfb77f8
areofyl <areofyl@users.noreply.github.com>
2026-07-13 21:00:14 -0700

    sync .config/nvim/lua/plugins/markdown.lua

--- gentoo/.config/nvim/lua/plugins/markdown.lua
+++ gentoo/.config/nvim/lua/plugins/markdown.lua
@@ -3,7 +3,22 @@ return {
     "OXY2DEV/markview.nvim",
     ft = "markdown",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {},
+    opts = {
+      markdown = {
+        indented_code_blocks = { enable = false },
+        list_items = {
+          shift_width = 4,
+          marker_minus = {
+            add_padding = false,
+            text = "—",
+          },
+          marker_star = {
+            add_padding = false,
+            text = "●",
+          },
+        },
+      },
+    },
   },
   {
     "dhruvasagar/vim-table-mode",
@@ -25,6 +40,17 @@ return {
           map("i", "<CR>", "<CR><cmd>AutolistNewBullet<CR>", { buffer = true })
           map("n", "o", "o<cmd>AutolistNewBullet<CR>", { buffer = true })
           map("n", "O", "O<cmd>AutolistNewBulletBefore<CR>", { buffer = true })
+          map("i", "<Space>", function()
+            local col = vim.fn.col(".") - 1
+            local line = vim.api.nvim_get_current_line()
+            local before = line:sub(1, col)
+            if before == "*" or before == "-" then
+              local keys = vim.api.nvim_replace_termcodes("<C-t>", true, false, true)
+              vim.api.nvim_feedkeys(keys .. " ", "n", false)
+            else
+              vim.api.nvim_feedkeys(" ", "n", false)
+            end
+          end, { buffer = true })
         end,
       })
     end,
