f490cdea1825fcb105e02f42e41c4a913132e3f3
areofyl <areofyl@users.noreply.github.com>
2026-07-13 19:36:47 -0700

    sync .config/nvim/lua/plugins/autopairs.lua

--- gentoo/.config/nvim/lua/plugins/autopairs.lua
+++ gentoo/.config/nvim/lua/plugins/autopairs.lua
@@ -4,6 +4,10 @@ return {
     event = "InsertEnter",
     opts = {
       modes = { insert = true, command = false, terminal = false },
+      mappings = {
+        -- disable * pairing so bullets and italic markers work in markdown
+        ["*"] = false,
+      },
     },
   },
 }
