3579d18f05af7098fff1548a88c079aaa1504808
areofyl <areofyl@users.noreply.github.com>
2026-07-19 17:00:25 -0700

    sync .config/nvim/init.lua

--- gentoo/.config/nvim/init.lua
+++ gentoo/.config/nvim/init.lua
@@ -21,6 +21,42 @@ vim.opt.splitbelow = true
 vim.opt.cursorline = true
 vim.opt.updatetime = 250
 
+-- force 4-space tabs on all filetypes (override ftplugins)
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end,
+})
+
+-- preserve indentation on blank lines when leaving insert mode
+vim.api.nvim_create_autocmd("InsertLeavePre", {
+  callback = function()
+    local line = vim.api.nvim_get_current_line()
+    if line:match("^%s+$") then
+      -- mark it so we can restore after vim strips it
+      vim.b._keep_indent_line = vim.fn.line(".")
+      vim.b._keep_indent_text = line
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    local lnum = vim.b._keep_indent_line
+    local text = vim.b._keep_indent_text
+    if lnum and text then
+      local cur = vim.fn.getline(lnum)
+      if cur == "" then
+        vim.fn.setline(lnum, text)
+      end
+    end
+    vim.b._keep_indent_line = nil
+    vim.b._keep_indent_text = nil
+  end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.hl.on_yank({ timeout = 150 }) end,
 })
