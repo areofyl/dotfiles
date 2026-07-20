bf398088fcb9ba30c8daf1d851f9bb953e385531
areofyl <areofyl@users.noreply.github.com>
2026-07-19 18:00:25 -0700

    sync .config/nvim/lsp/clangd.lua

--- gentoo/.config/nvim/lsp/clangd.lua
+++ gentoo/.config/nvim/lsp/clangd.lua
@@ -1,4 +1,13 @@
 return {
-  cmd = { "clangd", "--background-index", "--clang-tidy", "--completion-style=detailed", "--header-insertion=iwyu" },
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--completion-style=detailed",
+    "--header-insertion=iwyu",
+    "--function-arg-placeholders",
+    "--all-scopes-completion",
+    "--pch-storage=memory",
+  },
   filetypes = { "c", "cpp", "objc", "objcpp" },
 }
